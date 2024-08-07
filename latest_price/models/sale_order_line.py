523f006304327064ba5284d4b362dc452b6f3ad6
shom-odoo <shom@odoo.com>
2024-08-07 12:53:01 +0400

    done

--- latest_price/models/sale_order_line.py
+++ latest_price/models/sale_order_line.py
@@ -4,8 +4,8 @@ from odoo import fields, models, api
 class sale_order_line(models.Model):
 
     _inherit = 'sale.order.line'
-    colum = fields.Integer(string="testing", default=10)
-    latest_price = fields.Float(string='Latest Sale Price', compute="_compute_latest", readonly=True)
+
+    latest_price = fields.Float(string='Latest Sale Price', compute="_compute_latest", store=True)
 
 
 
@@ -14,14 +14,14 @@ class sale_order_line(models.Model):
             ('product_id', '=', product_id.id),
             ('order_id.partner_id', '=', partner_id.id),
             ('order_id.state', 'in', ['sale', 'done'])
-        ], order='id desc', limit=1)
+        ], order='create_date desc', limit=1)
 
         if records:
             return records.price_unit
         else:
             return 0
 
-    @api.depends('product_id')
+    @api.depends('product_template_id', 'order_id.partner_id')
     def _compute_latest(self):
         for record in self:
             if record.order_id.partner_id and record.product_id:
