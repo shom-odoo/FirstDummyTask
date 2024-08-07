2e4fce8b97273385100a8fbf1bbe6c39d40aecbf
shom-odoo <shom@odoo.com>
2024-08-07 08:35:00 +0400

    Adding SO

---
+++ latest_price/__manifest__.py
@@ -0,0 +1,7 @@
+{
+'name': 'Latest SO-PO Price',
+'depends' : ['base', 'sale_management', 'purchase'],
+'data':['views/latest_price_view.xml'],
+'application': False,
+'license': 'LGPL-3',
+}
---
+++ latest_price/models/sale_order_line.py
@@ -0,0 +1,30 @@
+from odoo import fields, models, api
+
+
+class sale_order_line(models.Model):
+
+    _inherit = 'sale.order.line'
+    colum = fields.Integer(string="testing", default=10)
+    latest_price = fields.Float(string='Latest Sale Price', compute="_compute_latest", readonly=True)
+
+
+
+    def _get_latest_price(self, partner_id, product_id):
+        records = self.search([
+            ('product_id', '=', product_id.id),
+            ('order_id.partner_id', '=', partner_id.id),
+            ('order_id.state', 'in', ['sale', 'done'])
+        ], order='id desc', limit=1)
+
+        if records:
+            return records.price_unit
+        else:
+            return 0
+
+    @api.depends('product_id')
+    def _compute_latest(self):
+        for record in self:
+            if record.order_id.partner_id and record.product_id:
+                record.latest_price = self._get_latest_price(record.order_id.partner_id, record.product_id)
+            else:
+                record.latest_price = 0
